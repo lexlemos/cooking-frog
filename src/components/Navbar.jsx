f6a859ea9f0bd833d181a129b7eec14d6249bafe
allex <allexlemos12@gmail.com>
2025-09-22 17:14:00 -0300

    projeto de receitas culinarias

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+
+import Searchbar from './Searchbar'
+import { ThemeContext } from '../context/ThemeContext';
+//styles
+import './Navbar.css'
+
+export default function Navbar(){
+    const { color } = useContext(ThemeContext)
+
+    return (
+
+        <div className='navbar' style={{background: color}}>
+            <nav>
+                <Link to="/" className='brand'>
+                <h1>Cooking Frog</h1>
+                </Link>
+                <Searchbar />
+                <Link to="/create" >Create Recipe</Link>
+            </nav>
+        </div>
+        
+    )
+}
